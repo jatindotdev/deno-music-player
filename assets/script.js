6ac43a1a359c7833528012f2aa834b6e57bec56c
Jatin Kumar <main.jatink@gmail.com>
2023-01-21 15:50:20 +0530

    [core] add keyboard shortcuts (seek) (arrow and num keys)

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,75 @@
-const albumImg = document.getElementById('album-img');
-const songName = document.getElementById('song-name');
-const inputField = document.getElementById('input');
-const searchButton = document.getElementById('search');
-const audioPlayer = document.querySelector('audio');
+const albumImg = document.getElementById("album-img");
+const songName = document.getElementById("song-name");
+const inputField = document.getElementById("input");
+const searchButton = document.getElementById("search");
+const audioPlayer = document.querySelector("audio");
+const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => `Digit${value}`);
 
-let searchQuery = '';
+let searchQuery = "";
 
 inputField.focus();
 
 const handleSearch = async () => {
   if (searchQuery.length > 0) {
-    const query = searchQuery.toLowerCase().split(' ').join('+');
+    const query = searchQuery.toLowerCase().split(" ").join("+");
     const res = await fetch(`/audio?query=${query}`);
     const data = await res.json();
 
-    albumImg.src = data['albumArt']['url'];
-    songName.textContent = data['title'];
-    audioPlayer.src = data['url'];
+    albumImg.src = data["albumArt"]["url"];
+    songName.textContent = data["title"];
+    audioPlayer.src = data["url"];
     audioPlayer.play();
-    inputField.value = '';
+    inputField.value = "";
   }
 };
 
-document.addEventListener('keyup', async (event) => {
-  if (event.code === 'Slash' && document.activeElement !== inputField) {
-    inputField.focus();
+document.addEventListener("keyup", async (event) => {
+  if (event.code === "Escape") {
+    inputField.blur();
   }
 
-  if (event.code === 'Enter' && document.activeElement === inputField) {
-    handleSearch();
-  }
+  if (document.activeElement !== inputField) {
+    if (event.code === "Slash") {
+      inputField.focus();
+    }
 
-  if (event.code === 'Space' && document.activeElement !== inputField) {
-    try {
-      if (audioPlayer.paused) {
-        await audioPlayer.play();
-      } else {
-        audioPlayer.pause();
+    if (digits.includes(event.code)) {
+      audioPlayer.currentTime =
+        (digits.indexOf(event.code) / 10) * audioPlayer.duration;
+    }
+
+    if (event.code === "ArrowRight") {
+      if (audioPlayer.duration - (audioPlayer.currentTime + 10) < 0) return;
+      audioPlayer.currentTime += 10;
+    }
+
+    if (event.code === "ArrowLeft") {
+      if (audioPlayer.currentTime - 10 < 0) return;
+      audioPlayer.currentTime -= 10;
+    }
+
+    if (event.code === "Space") {
+      try {
+        if (audioPlayer.paused) {
+          await audioPlayer.play();
+        } else {
+          audioPlayer.pause();
+        }
+      } catch {
+        // No media
       }
-    } catch {
-      // No media
+    }
+  }
+
+  if (document.activeElement === inputField) {
+    if (event.code === "Enter") {
+      handleSearch();
     }
   }
 });
 
-inputField.addEventListener('input', (e) => {
+inputField.addEventListener("input", (e) => {
   searchQuery = e.currentTarget.value.trim();
 });
 
-searchButton.addEventListener('click', handleSearch);
+searchButton.addEventListener("click", handleSearch);
